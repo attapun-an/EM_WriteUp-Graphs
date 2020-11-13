a0e0fccc7bcc3c6e8f1aa50c114cfd59bb4377dc
Your Name <attapunanivat@gmail.com>
2020-11-14 02:08:47 +0700

    pun finished graphs

--- EM_tree_plot.R
+++ EM_tree_plot.R
@@ -5,21 +5,82 @@ library(ggrepel)
 
 # import and process data ----
 tree_data <- read.csv("TreeData.csv") %>% slice(-211) %>%                       # removes empty row
-  rename(Plot_Code = ï..Plot_Code) %>%                                          # renames header which didn't import correctly
+  rename(Plot_code = ï..Plot_Code) %>%                                          # renames header which didn't import correctly
   dplyr::select(-c(X, X.1))                                                     # removes empty columns 
 
 tree_data_Asia <- tree_data %>% 
-  dplyr::filter(Plot_Code %in% c("SG01", "SG02", "THA01")) %>%                  # filter only plots in Asia
+  dplyr::filter(Plot_code %in% c("SG01", "SG02", "THA01")) %>%                  # filter only plots in Asia
   mutate(Biomass = as.numeric(as.character(Biomass)))                           # turn biomass into a number instead of factor
 
+# The raw data for Europe was broken (had commas instead of decimal points for biomass)
 tree_data_Eurp <- tree_data %>% 
-  dplyr::filter(Plot_Code %in% c("SK01", "SK02")) %>%                           # filter only plots in Europe
+  dplyr::filter(Plot_code %in% c("SK01", "SK02")) %>%                           # filter only plots in Europe
   separate(., Biomass, into = c("Bio1", "Bio2"), sep = ",") %>%                 # Split by comma into two variables
-  mutate(Biomass = as.numeric(paste(Bio1,".",Bio2, sep = ""))) %>%              # turn them into numbers and convert to tonnes
+  mutate(Biomass = as.numeric(paste(Bio1,".",Bio2, sep = ""))) %>%              # Combine them again as numbers
   dplyr::select(-c("Bio1", "Bio2"))
   
-tree_data <- rbind(tree_data_Asia, tree_data_Eurp) %>%                          # combine the two data sets
-  mutate(Biomass = Biomass/1000) %>%                                            # convert to tonnes 
-  group_by(Plot_Code)
+tree_data <- rbind(tree_data_Asia, tree_data_Eurp) %>%                          # combine the two data 
+  mutate(Country = case_when(
+    grepl("SK", Plot_code) ~ "SK",
+    grepl("SG", Plot_code) ~ "SG",
+    grepl("THA", Plot_code) ~ "THA"),
+    Biomass = Biomass/1000) %>%                                                 # from kg to tonnes
+    group_by(Plot_code) %>% 
+    mutate(Biomass_per_plot = sum(Biomass)*5.09294626942)                       # sum biomass for each plot, scale data up to biomass per hectare
 
-plot_averages <- 
+
+#  Generate plot data ----
+
+# biomass per plot 
+p_Biomass_vs_Site <- tree_data %>% 
+   dplyr::select(Plot_code, Biomass_per_plot, Country) %>%                      # removed unneeded columns
+   distinct()                                                                   # only keep unique rows
+
+# mean biomass for each plot  
+p_Biomass_within_site <- tree_data %>%
+   dplyr::select(Plot_code, Biomass, Country, Biomass_per_plot) 
+   
+
+# colors
+colr <- c("#f4a261","#2a9d8f","#e9c46a")
+
+# Generate plots ----
+
+(graph_Biomass_per_plot <- ggplot(p_Biomass_vs_Site, 
+                                 aes(x = reorder(Plot_code, -Biomass_per_plot),
+                                     y = Biomass_per_plot, fill = Country))+
+  geom_bar(stat = "Identity", alpha = 0.75)+
+  theme_bw()+
+  scale_fill_manual(values = colr)+
+  labs(x= "\n Plot code", y = "Biomass per plot (tonnes/ha) \n")+
+  theme(axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 12), 
+        plot.title = element_text(size = 14, hjust = 0.5, face = 'bold'), 
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , 'cm'), 
+        legend.position = '0')
+ )
+  
+  
+(graph_Biomass_per_plot <- ggplot(p_Biomass_within_site, aes(x = reorder(Plot_code, -Biomass_per_plot),y = Biomass, fill = Country, alpha = 1 ))+
+  geom_violin()+ 
+  geom_point(aes(alpha = 0.1))+
+  # geom_count(inherit.aes = TRUE,)+
+  # geom_dotplot(binaxis="y", dotsize = 0.1, stackdir = "center")+
+  # geom_jitter(width = 0.2, aes(alpha = 0.4, colour = Country, size = 2))+
+  theme_bw()+
+  scale_fill_manual(values = colr)+
+  scale_color_manual(values = colr)+
+  labs(x= "\n Plot code", y = "Mean Biomass (tonnes/ha) \n")+
+  theme(axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 12), 
+        plot.title = element_text(size = 14, hjust = 0.5, face = 'bold'), 
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , 'cm'), 
+        legend.position = '0')
+    )
+
+
+  
+  
+  
+  
+
